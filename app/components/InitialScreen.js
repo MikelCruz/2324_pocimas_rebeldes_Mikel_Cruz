2eede7c0ab27c17a6d8219ffba4f18e7a2703196
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-02-06 09:17:10 +0100

    [NEW] Funcionalidad de InitialScreen creada - Recogida de datos y boton de cambio de pestañas (error en recogida de datos

--- app/components/InitialScreen.js
+++ app/components/InitialScreen.js
@@ -1,10 +1,32 @@
 import React from "react";
+import axios from "axios";
+
+const getData = async () => {
+  try{
+    console.log("Entra en getData");
+    const url = "https://gist.github.com/Oskar-Dam/ad2c96601e79ad108227bc25f90e4e53"
+    const data = await axios.get(url);
+    return data
+
+  } catch (error) {
+    console.error(error)
+  }
+} 
 
 
 const InitialScreen = () => {
+
+  const enterTheGame = () => {
+    console.log("entra en el boton");
+    const data = getData();
+    console.log("Datos:")
+    console.log(data);
+  }
+  
   return (
     <div>
       <h1 style={{color: "white"}}> LAS POCIMAS REBELDES</h1>
+      <button onClick={() => { enterTheGame()} }> ENTER </button>
     </div>
   );
 }
