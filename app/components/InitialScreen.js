a5e0c32b6f7b4e15d83d58af46092db80555904c
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-02-06 09:43:36 +0100

    [UPDATE] Funcionalidad de initialScreen completada - falta introducir el componente deseado

--- app/components/InitialScreen.js
+++ app/components/InitialScreen.js
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 
-const getData = async () => {
-  try{
 
-    console.log("Entra en getData");
-    const url = "https://gist.githubusercontent.com/Oskar-Dam/ad2c96601e79ad108227bc25f90e4e53/raw/25dc0198b2aaa85f0b5583978a0c6772cab63aba/Potions.js"
-    const data = await axios.get(url);
-    return data.data
-
-  } catch (error) {
-    console.error(error)
-  }
-} 
 
 
 const InitialScreen = () => {
 
+  // Variables locales 
+  const [recivedData, setRecivedData] = useState([]); 
+  const [isInTheGame, setIsInTheGame] = useState(false); 
+
+  // Use Effect para recargar datos
+  useEffect(() => { 
+    console.log("Esta en el juego? " + isInTheGame);
+
+    console.log("Pociones Redicibdas: ");
+    console.log(recivedData);
+
+  }, [isInTheGame, recivedData])
+
+  const getData = async () => {
+    try{
+  
+      console.log("Entra en getData");
+      const url = "https://gist.githubusercontent.com/Oskar-Dam/ad2c96601e79ad108227bc25f90e4e53/raw/25dc0198b2aaa85f0b5583978a0c6772cab63aba/Potions.js"
+      const data = await axios.get(url);
+      setIsInTheGame(true)
+      return data.data
+  
+    } catch (error) {
+      console.error(error)
+      setIsInTheGame(false)
+    }
+  } 
+
   const enterTheGame = async () => {
 
-    const data = await getData();
-    console.log(data);
+    setRecivedData( await getData());
   }
   
   return (
     <div>
-      <h1 style={{color: "white"}}> LAS POCIMAS REBELDES</h1>
-      <button onClick={() => { enterTheGame()} }> ENTER </button>
+      {!isInTheGame && (
+      <div>  
+        <h1 style={{color: "white"}}> LAS POCIMAS REBELDES</h1>
+        <button onClick={() => { enterTheGame()} }> ENTER </button>
+      </div>
+      )}
+      
+      {isInTheGame && (
+        <h1> ESTOY DENTRO</h1>
+
+      )}
     </div>
   );
 }
